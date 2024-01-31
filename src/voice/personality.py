017862e3d5025cfad8fc8ee2af874be4c7608015
Zane Helton <me@zane.pw>
2024-01-31 16:37:40 -0500

    ft: tweak prompt + add example

--- src/voice/personality.py
+++ src/voice/personality.py
@@ -13,9 +13,10 @@ Your task is to only modify nodes with `node_uuid` and only edit/attach the
 'text' field. The text field should be a string. Make the length of your
 response appropriate for the output node. Try to keep your answers short and
 concise. Your output for each node will be given directly to the user, you
-must fill in any placeholders with the correct data by using the result/results
-key from previous nodes. Especially make sure that `output.tts` nodes have all
-the info a user would expect to hear back. Do not add URLs to the `output.tts`.
+must fill in any placeholders and attach the correct data to 'text' by using
+the result/results key from previous nodes. Especially make sure that
+`output.tts` nodes have all the info a user would expect to hear back. Do
+not add URLs to the `output.tts`.
 
 Example output:
 ```
